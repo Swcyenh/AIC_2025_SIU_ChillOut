0fb263daf59ffdd9b9c49c81d07a4e9b42962e86
Swcyenh <tranquanghuyk15@siu.edu.vn>
2025-07-02 12:33:17 +0700

    Add Shot detection and extraction code

--- Shot_Detection/AutoShot/Infer/split_sceen_autoshot.py
+++ Shot_Detection/AutoShot/Infer/split_sceen_autoshot.py
@@ -1,28 +1,3 @@
-from shot_detecion_selector import ShotDetection
-from io_setup import setup_video_path, SceneJsonLoader, CutKeyFrameLoader
-import os
-from typing import Dict, List, Any
-import cv2
-import json
-import numpy as np
-input_dir = "/dataset/AIC2024/original_dataset/0/videos/"
-all_video_paths = setup_video_path(input_dir)
-for video in all_video_paths:
-    print(video)
-model = ShotDetection('autoshot')
-prediction_scenes = model.run_model(video_path_dict=all_video_paths)
-sceneJson_dir = "/workspace/competitions/AIC_2025/SIU_ChillOut/Swcyen/SceneJson"
-os.makedirs(sceneJson_dir, exist_ok=True)
-json_handling = SceneJsonLoader(
-    prediction_scenes,
-    sceneJson_dir
-)
-json_handling.save_results()
-# keyframe_dir = "/dataset/AIC_2025/SIU_ChillOut/Frames/Old_frame_2024"
-# keyframe_handler = CutKeyFrameLoader(
-#     sceneJson_dir,
-#     keyframe_dir
-# )
-# keyframe_handler.extract_keyframes(
-#     all_video_paths
-# )
+version https://git-lfs.github.com/spec/v1
+oid sha256:4580f588cd52d52e75ecf88b74d543d2bcc90b849b6e62476b77e083a968324e
+size 912
