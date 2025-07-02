b38af797da2ca9ee9d2621c67af0b47c639846a5
Swcyenh <tranquanghuyk15@siu.edu.vn>
2025-07-02 12:31:23 +0700

    Add Feature extraction code

---
+++ Feature_extraction/Laion_extractor.py
@@ -0,0 +1,19 @@
+import sys
+sys.append("/workspace/competitions/AIC_2025/SIU_ChillOut/Main_Source/Utils")
+from tqdm import tqdm
+import os
+from PIL import Image
+import numpy as np
+from Class.Laion import Laion
+
+model = Laion()
+
+main_path = "/dataset/AIC_2025/SIU_ChillOut/Frames/Old_frame_2024/video"
+for folder in tqdm(sorted(os.listdir(main_path))):
+    folder_path = main_path + "/" + folder + "/"
+    result = []
+    for keyframes in tqdm(sorted(os.listdir(folder_path))):
+        keyframes_path = folder_path + keyframes
+        feet = model.get_image_features(keyframes_path)
+        result.append(feet)
+    np.save(f"/dataset/AIC_2025/SIU_ChillOut/Features/Old_features_2024/Laion/{folder}.npy", result)
